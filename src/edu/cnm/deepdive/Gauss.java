75cb7a27b2de9dcf16cb0a05b07f13b457dac570
rsena9 <resena133@gmail.com>
2019-09-25 10:10:34 -0600

    Added exception handling

--- src/edu/cnm/deepdive/Gauss.java
+++ src/edu/cnm/deepdive/Gauss.java
@@ -2,12 +2,23 @@ package edu.cnm.deepdive;
 
 public class Gauss {
 
-  public static void main(String[] args) {
-    int sum = 0;
-    for (int i = 1; i <= 100; i++) {
-      sum += i;
-    }
-    System.out.println(sum);
+  private static final int DEFAULT_UPPER_BOUND = 100;
 
+  public static void main(String[] args) {
+
+    try {
+      int upperBound = (args.length>0) ? Integer.parseInt(args[0]): DEFAULT_UPPER_BOUND;
+      if (upperBound <=0) {
+      throw new IllegalArgumentException();
+      }
+      long sum = 0;
+      for (long i = 1; i <= upperBound; i++) {
+        sum += i;
+      }
+      System.out.println(sum);
+    } catch (IllegalArgumentException e) {
+      System.out.printf("Upper bound must be an integer in the range 1...%d%n", Integer.MAX_VALUE);
+    }
   }
+
 }
